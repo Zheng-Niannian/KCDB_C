13c53eee930d1d49c6a1e84a1059bc7fda24a386
Zheng <79322267+Zheng-Niannian@users.noreply.github.com>
2024-04-08 01:05:48 +0800

    Create event_handler.c

---
+++ server/event_handler.c
@@ -0,0 +1,48 @@
+#include "event_handler.h"
+
+SocketHandlerList list;
+
+void $__initHandlerList(struct socket_handler_list *it);
+void $__handleMessage(struct socket_handler_list *it,ClientTransferState *state,PacketPayload *payload);
+
+void initEventHandler(void){
+    $__initHandlerList(&list);
+}
+void setDataHandler(int dataType, void (*callback)(ClientTransferState *state,PacketPayload *payload)) {
+    if(list.handlers[dataType].exist){
+        printf("target data type has already bind a handler\n");
+        return;
+    }
+    SocketDataHandler handler={
+            .exist=1,
+            .callback=callback
+    };
+    printf("bind handler for dataType:%d\n",dataType);
+    list.handlers[dataType]=handler;
+}
+
+void handleMessage(ClientTransferState *state,PacketPayload *payload){
+    if(payload==NULL){
+        fprintf(stdout,"payload must not be NULL\n");
+        return;
+    }
+    printf("payload data type:%u\n",payload->dataType);
+    if(payload->dataType<0||payload->dataType>QUERY_KEY_RESULT){
+        fprintf(stdout,"unsupported data type\n");
+        return;
+    }
+    list.handleMessage(&list,state,payload);
+}
+
+void $__initHandlerList(struct socket_handler_list *it){
+    memset((void*)it->handlers,0,sizeof(SocketDataHandler)*SOCKET_DATA_HANDLER_SIZE);
+    it->handleMessage=$__handleMessage;
+}
+
+void $__handleMessage(struct socket_handler_list *it,ClientTransferState *state,PacketPayload *payload){
+    if(!it->handlers[payload->dataType].exist){
+        printf("no suitable handler for dataType:%d,cancel handle operation.\n", payload->dataType);
+        return;
+    }
+    it->handlers[payload->dataType].callback(state,payload);
+}
