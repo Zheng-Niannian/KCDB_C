8fd64fa86908ecbe2f2b6352abe096e71b85a65d
Zheng <79322267+Zheng-Niannian@users.noreply.github.com>
2024-04-08 01:11:04 +0800

    Create transfer.h

---
+++ server/transfer.h
@@ -0,0 +1,38 @@
+#ifndef KVFILESERVER_TRANSFER_H
+#define KVFILESERVER_TRANSFER_H
+
+#include"data_type.h"
+#include<stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+typedef struct client_transfer_state {
+    int socket;
+    uint32 clientId;
+    pthread_t threadId;
+    uint32 messageType;
+    uint32 unknownMessageLength;
+    uint32 unknownMessageType;
+    uint32 messageLength;
+    uint32 receivedLength;
+    uint32 messageId;
+    char busy;
+    char connect;
+    char* buffer;
+}ClientTransferState;
+
+void initServer(void);
+
+void beginServer(void);
+
+void *serializeTransferData(void* src, uint32 len, enum TransferPacketType type);
+
+int sendSerializeData(ClientTransferState*state,void *serializeData,uint32 beforeSerializeLength);
+
+PacketPayload deserializeActualData(ClientTransferState*state,void* header, uint32 len);
+
+
+void cleanResources();
+
+#endif //KVFILESERVER_TRANSFER_H
