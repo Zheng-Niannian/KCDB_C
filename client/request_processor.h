c892cfb4656de75d14730f3a80729af459af6d06
Zheng <79322267+Zheng-Niannian@users.noreply.github.com>
2024-04-08 01:20:04 +0800

    Create request_processor.h

---
+++ client/request_processor.h
@@ -0,0 +1,12 @@
+#ifndef KVFILECLIENT_REQUEST_PROCESSOR_H
+#define KVFILECLIENT_REQUEST_PROCESSOR_H
+
+#include"transfer.h"
+#include"event_handler.h"
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+
+void initRequestProcessor(void);
+
+#endif //KVFILECLIENT_REQUEST_PROCESSOR_H
