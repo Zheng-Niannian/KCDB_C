7c8c7e49f8404bace3f57727874d324bf35f5de6
Zheng <79322267+Zheng-Niannian@users.noreply.github.com>
2024-04-15 21:00:13 +0800

    Update data_type.h

--- server/data_type.h
+++ server/data_type.h
@@ -1,23 +1,28 @@
 #ifndef KVFILESERVER_DATA_TYPE_H
 #define KVFILESERVER_DATA_TYPE_H
 
+#include <bits/stdint-intn.h>
+#include<stdio.h>
+
 #define SOCKET_BUFFER_SIZE (1024)
 #define SOCKET_DATA_HANDLER_SIZE (20)
 
-typedef unsigned int uint32;
-
-enum TransferPacketType{
-    NOT_WAIT,LOGIN_REQUEST,LOGIN_RESULT,QUERY_KEY_REQUEST,QUERY_KEY_RESULT
+enum CommandType{
+    NOT_WAIT,LOGIN_REQUEST,LOGIN_RESULT,FIND_REQUEST,FIND_RESULT,SET_REQUEST,SET_RESULT,UPDATE_REQUEST,UPDATE_RESULT,
+    FIND_LESS_REQUEST,FIND_LESS_RESULT,FIND_MORE_REQUEST,FIND_MORE_RESULT,DELETE_REQUEST,DELETE_RESULT,HELP_COMMAND
 };
 
+
+
 typedef struct {
-    uint32 dataType;
+    int32_t dataType;
 }PacketHeader;
 
 typedef struct{
     void *src;
-    uint32 actualLen;
-    uint32 dataType;
+    int32_t actualLen;
+    int32_t dataType;
+    FILE *fp;
 }PacketPayload;
 
 typedef struct{
@@ -27,18 +32,26 @@ typedef struct{
 
 typedef struct{
     int loginResult;
-    uint32 clientId;
+    int32_t clientId;
 }LoginResult;
 
-typedef struct{
-    uint32 keyLength;
-    void *key;
-}QueryKeyRequest;
+//typedef struct{
+//    int32_t keyLength;
+//    void *key;
+//}QueryKeyRequest;
+//
+//typedef struct{
+//    int32_t valueLength;
+//    void *value;
+//}QueryKeyResult;
 
 typedef struct{
-    uint32 valueLength;
-    void *value;
-}QueryKeyResult;
+    int totalLength;//sizeof(KeyLength)+sizeof(valueLength)+KeyContent+ValueContent
+    int keyLength;
+    int valueLength;
+    //key content
+    //value content;
+}TransferCommand;
 
 
 
