00523b3f43de8b8640cd309c95194aa21662fed5
Zheng <79322267+Zheng-Niannian@users.noreply.github.com>
2024-04-15 17:45:01 +0800

    Update transfer.h

--- server/transfer.h
+++ server/transfer.h
@@ -9,28 +9,34 @@
 
 typedef struct client_transfer_state {
     int socket;
-    uint32 clientId;
+    int32_t clientId;
     pthread_t threadId;
-    uint32 messageType;
-    uint32 unknownMessageLength;
-    uint32 unknownMessageType;
-    uint32 messageLength;
-    uint32 receivedLength;
-    uint32 messageId;
+    int32_t messageType;
+    int32_t unknownMessageLength;
+    int32_t unknownMessageType;
+    int32_t messageLength;
+    int32_t receivedLength;
+    int32_t messageId;
     char busy;
     char connect;
     char* buffer;
+    FILE *fp;
+    char filename_buffer[SOCKET_BUFFER_SIZE];
 }ClientTransferState;
+
+#define TRANSFER_MEMORY_BUFFER_MAX_SIZE     (2*SOCKET_BUFFER_SIZE)
 
 void initServer(void);
 
-void beginServer(void);
+void *serializeTransferData(void* src, int32_t len, enum CommandType type);
 
-void *serializeTransferData(void* src, uint32 len, enum TransferPacketType type);
+int sendSerializeData(ClientTransferState*state,void *serializeData,int32_t beforeSerializeLength);
 
-int sendSerializeData(ClientTransferState*state,void *serializeData,uint32 beforeSerializeLength);
+int sendFileData(ClientTransferState*state,const char* filename);
 
-PacketPayload deserializeActualData(ClientTransferState*state,void* header, uint32 len);
+int sendRawData(ClientTransferState*state,const char*buffer,int32_t len);
+
+PacketPayload deserializeActualData(ClientTransferState*state,void* header, int32_t len);
 
 
 void cleanResources();
