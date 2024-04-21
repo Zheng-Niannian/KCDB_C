7bfe4a9c0fd9db88de4bd05159f8e68b71c793c9
Zheng <79322267+Zheng-Niannian@users.noreply.github.com>
2024-04-22 03:19:05 +0800

    Update main.c

--- server/main.c
+++ server/main.c
@@ -1,8 +1,10 @@
 #include"transfer.h"
 #include"event_hander.h"
 #include"request_processor.h"
+#include"logger.h"
 
 int main() {
+    init_logger();
     initEventHandler();
     initRequestProcessor();
     initServer();
