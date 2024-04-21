207d8a325a160f09a989a80ac5c76f41e4e2ad11
Zheng <79322267+Zheng-Niannian@users.noreply.github.com>
2024-04-22 03:20:39 +0800

    Update request_processor.c

--- server/request_processor.c
+++ server/request_processor.c
@@ -1,48 +1,29 @@
 #include"request_processor.h"
 
-//void handleQueryKeyResult(ClientTransferState *state,PacketPayload *payload){
-//    QueryKeyResult *result=(QueryKeyResult*)payload->src;
-//    char *resultBuffer=(char*)malloc(result->valueLength+1);
-//    if(!resultBuffer){
-//        fprintf(stdout,"unable to allocate memory for QueryKeyResult.value\n");
-//        return;
-//    }
-//    memset(resultBuffer,0,result->valueLength+1);
-//    char *dest= (char *) &result->valueLength;
-//    memcpy(resultBuffer,dest+sizeof(result->valueLength),result->valueLength);
-//    fprintf(stdout,"QueryKeyResult.value:%s\n",resultBuffer);
-//
-//
-//    free(resultBuffer);
-//}
-//
-//void handleQueryKeyRequest(ClientTransferState *state,PacketPayload *payload){
-//    QueryKeyRequest *request=(QueryKeyRequest *)payload->src;
-//    char *resultBuffer=(char*)malloc(request->keyLength + 1);
-//    if(!resultBuffer){
-//        fprintf(stdout,"unable to allocate memory for QueryKeyResult.value\n");
-//        return;
-//    }
-//    memset(resultBuffer, 0, request->keyLength + 1);
-//    char *dest= (char *) &request->keyLength;
-//    memcpy(resultBuffer, dest+sizeof(request->keyLength), request->keyLength);
-//    fprintf(stdout,"QueryKeyRequest.value:%s\n",resultBuffer);
-//
-//    char returnBuffer[SOCKET_BUFFER_SIZE]={0};
-//    sprintf(returnBuffer,"server_received_query_key_request:%s",resultBuffer);
-//    QueryKeyResult result={
-//            strlen(returnBuffer),returnBuffer
-//    };
-//    int len=sizeof(result.valueLength)+result.valueLength;
-//    char *data=serializeTransferData(&result,len,QUERY_KEY_RESULT);
-//    sendSerializeData(state,data,len);
-//
-//    free(resultBuffer);
-//    free(data);
-//    fprintf(stdout,"server process QueryKeyRequestSuccess\n");
-//}
+art_tree $__art_tree;
 
-void handleFindRequest(ClientTransferState *state,PacketPayload *payload){
+void cleanupTransferCommandPayload(TransferCommandPayload* payload){
+    if(payload->key!=NULL){
+//        free(payload->key);
+        payload->key=NULL;
+        payload->valueLength=0;
+    }
+    if(payload->value!=NULL){
+//        free(payload->value);
+        payload->value=NULL;
+        payload->valueLength=0;
+    }
+    payload->flag=0;
+    free(payload);
+}
+
+TransferCommandPayload* resolveTransferCommand(ClientTransferState *state,PacketPayload *payload){
+    TransferCommandPayload* transferCommandPayload=malloc(sizeof(TransferCommandPayload));
+    if(!transferCommandPayload){
+        log_error("cannot malloc memory for transfer command payload,abort\n");
+        return NULL;
+    }
+    memset(transferCommandPayload,0,sizeof(TransferCommandPayload));
     TransferCommand transferCommand;
     memset(&transferCommand,0,sizeof(TransferCommand));
     if(payload->fp){
@@ -51,42 +32,43 @@ void handleFindRequest(ClientTransferState *state,PacketPayload *payload){
 
         FILE *fp=fopen(state->filename_buffer,"rb+");
         if(!fp){
-            fprintf(stdout,"cannot load packet from file:%s\n",state->filename_buffer);
+            log_error("cannot load packet from file:%s\n",state->filename_buffer);
             memset(state->filename_buffer,0,SOCKET_BUFFER_SIZE);
-            return;
+            return transferCommandPayload;
         }
         memset(state->filename_buffer,0,SOCKET_BUFFER_SIZE);
 
         PacketHeader header;
         fread(&header,sizeof(PacketHeader),1,fp);
-//        if(header.dataType!=FIND_REQUEST){
-//            fprintf(stdout,"invalid data type:%d\n",header.dataType);
-//            return;
-//        }
         fread(&transferCommand,sizeof(transferCommand.totalLength),3,fp);
-        fprintf(stdout,"read transfer command from file,totalLength:%d, keyLength:%d, valueLength:%d\n"
+        log_info("read transfer command from file,totalLength:%d, keyLength:%d, valueLength:%d\n"
                 ,transferCommand.totalLength,transferCommand.keyLength,transferCommand.valueLength);
         if(transferCommand.keyLength>0){
-            char *key_buffer=malloc(transferCommand.keyLength+1);
+            unsigned char *key_buffer=malloc(transferCommand.keyLength+1);
             memset(key_buffer,0,transferCommand.keyLength+1);
             if(!key_buffer){
-                fprintf(stdout,"cannot allocate memory for key\n");
-                return;
+                log_error("cannot allocate memory for key\n");
+                return transferCommandPayload;
             }
+            transferCommandPayload->keyLength=transferCommand.keyLength;
+            transferCommandPayload->key=key_buffer;
             fread(key_buffer,1,transferCommand.keyLength,fp);
-            fprintf(stdout,"key:%s\n",key_buffer);
-            free(key_buffer);
+            log_info("key:%s\n",key_buffer);
+//            free(key_buffer);
         }
         if(transferCommand.valueLength>0){
-            char *value_buffer=malloc(transferCommand.valueLength+1);
+            unsigned char *value_buffer=malloc(transferCommand.valueLength+1);
             memset(value_buffer,0,transferCommand.keyLength+1);
             if(!value_buffer){
-                fprintf(stdout,"cannot allocate memory for value\n");
-                return;
+                log_error("cannot allocate memory for value\n");
+                return transferCommandPayload;
             }
+            transferCommandPayload->valueLength=transferCommand.valueLength;
+            transferCommandPayload->value=value_buffer;
             fread(value_buffer, 1, transferCommand.valueLength, fp);
-            fprintf(stdout,"value:%s\n",value_buffer);
-            free(value_buffer);
+            log_info("value:%s\n",value_buffer);
+
+//            free(value_buffer);
         }
         fclose(fp);
         fprintf(stdout,"finish process find_request from file\n");
@@ -94,40 +76,230 @@ void handleFindRequest(ClientTransferState *state,PacketPayload *payload){
     }
     else{
         transferCommand=*(TransferCommand *)payload->src;
-        fprintf(stdout,"read transfer command from memory,totalLength:%d, keyLength:%d, valueLength:%d\n"
+        log_success("read transfer command from memory,totalLength:%d, keyLength:%d, valueLength:%d\n"
                 ,transferCommand.totalLength,transferCommand.keyLength,transferCommand.valueLength);
         char* key=((char*)payload->src)+sizeof(transferCommand.totalLength)*3;
         if(transferCommand.keyLength>0){
             char *key_buffer=(char*)malloc(sizeof(char)*transferCommand.keyLength+1);
             if(!key_buffer){
-                fprintf(stdout,"cannot allocate memory for key\n");
+                log_error("cannot allocate memory for key\n");
+                return transferCommandPayload;
             }
             memset(key_buffer,0,transferCommand.keyLength+1);
             memcpy(key_buffer,key,transferCommand.keyLength);
-            fprintf(stdout,"key:%s\n",key_buffer);
-            free(key_buffer);
+            log_info("key:%s\n",key_buffer);
+            transferCommandPayload->keyLength=transferCommand.keyLength;
+            transferCommandPayload->key=key_buffer;
+//            free(key_buffer);
         }
         char *value=((char*)payload->src)+sizeof(transferCommand.totalLength)*3+transferCommand.keyLength;
         if(transferCommand.valueLength>0){
             char *value_buffer=(char*)malloc(sizeof(char) * transferCommand.valueLength + 1);
             if(!value_buffer){
-                fprintf(stdout,"cannot allocate memory for key\n");
+                log_error("cannot allocate memory for key\n");
+                return transferCommandPayload;
             }
+            transferCommandPayload->valueLength=transferCommand.valueLength;
+            transferCommandPayload->value=value_buffer;
             memset(value_buffer, 0, transferCommand.valueLength + 1);
             memcpy(value_buffer, value, transferCommand.valueLength);
-            fprintf(stdout, "value:%s\n", value_buffer);
-            free(value_buffer);
+            log_info( "value:%s\n", value_buffer);
+//            free(value_buffer);
         }
 
-        fprintf(stdout,"finish process find_request from memory\n");
+//        fprintf(stdout,"finish process find_request from memory\n");
     }
+    transferCommandPayload->flag=1;
+    log_success("Resolve Transfer Command:key:%s, value:%s\n",transferCommandPayload->key,transferCommandPayload->value);
+    return transferCommandPayload;
+}
+
+void handleFindRequest(ClientTransferState *state,PacketPayload *payload){
+    TransferCommandPayload* transferCommandPayload= resolveTransferCommand(state,payload);
+    log_info("IN FUNCTION,ret=%p\n",transferCommandPayload);
+    if(transferCommandPayload==NULL)return;
+
+    if(!transferCommandPayload->flag){
+        cleanupTransferCommandPayload(transferCommandPayload);
+        log_error("cannot resolve transfer command payload,abort\n");
+        return;
+    }
+    log_info("FLAG:TRUE\n");
+
+    if(transferCommandPayload->key==NULL){
+        log_error("In find request,field key should not be null\n");
+        cleanupTransferCommandPayload(transferCommandPayload);
+        return;
+    }
+
+    if(transferCommandPayload->value!=NULL){
+        log_warning("In find request,field value should be null\n");
+    }
+    log_info("BEGIN_FIND");
+    void* result=art_search(&$__art_tree,transferCommandPayload->key,transferCommandPayload->keyLength);
+
+    log_success("FindRequest,key:%s,value:%s\n",transferCommandPayload->key,result);
+
+//    cleanupTransferCommandPayload(transferCommandPayload);
+
+}
+
+void handleSetRequest(ClientTransferState *state,PacketPayload *payload){
+    TransferCommandPayload* transferCommandPayload= resolveTransferCommand(state,payload);
+    log_info("IN FUNCTION,ret=%p\n",transferCommandPayload);
+    if(transferCommandPayload==NULL)return;
+    if(!transferCommandPayload->flag){
+        cleanupTransferCommandPayload(transferCommandPayload);
+        log_error("cannot resolve transfer command payload,abort\n");
+        return;
+    }
+
+    log_info("FLAG:TRUE\n");
+
+    if(transferCommandPayload->key==NULL){
+        log_error("In set request,field key should not be null\n");
+        cleanupTransferCommandPayload(transferCommandPayload);
+        return;
+    }
+
+    if(transferCommandPayload->value==NULL){
+        log_error("In set request,field value should not be null\n");
+        cleanupTransferCommandPayload(transferCommandPayload);
+        return;
+    }
+    log_info("BEGIN_SET");
+    void* result=art_insert_no_replace(&$__art_tree,transferCommandPayload->key,transferCommandPayload->keyLength,(void*)transferCommandPayload->value);
+
+    log_success("SetRequest,key:%s,old value:%s,new value:%s\n",transferCommandPayload->key,result,(char*)transferCommandPayload->value);
+
+
+//    cleanupTransferCommandPayload(transferCommandPayload);
+}
+
+void handleUpdateRequest(ClientTransferState *state,PacketPayload *payload){
+    TransferCommandPayload* transferCommandPayload= resolveTransferCommand(state,payload);
+    log_info("IN FUNCTION,ret=%p\n",transferCommandPayload);
+    if(transferCommandPayload==NULL)return;
+    if(!transferCommandPayload->flag){
+        cleanupTransferCommandPayload(transferCommandPayload);
+        log_error("cannot resolve transfer command payload,abort\n");
+        return;
+    }
+
+    log_info("FLAG:TRUE\n");
+
+    if(transferCommandPayload->key==NULL){
+        log_error("In update request,field key should not be null\n");
+        cleanupTransferCommandPayload(transferCommandPayload);
+        return;
+    }
+
+    if(transferCommandPayload->value==NULL){
+        log_error("In update request,field value should not be null\n");
+        cleanupTransferCommandPayload(transferCommandPayload);
+        return;
+    }
+    log_info("BEGIN_UPDATE");
+    void* result=art_insert(&$__art_tree,transferCommandPayload->key,transferCommandPayload->keyLength,(void*)transferCommandPayload->value);
+    log_success("UpdateRequest,key:%s,old value:%s,new value:%s\n",transferCommandPayload->key,result,transferCommandPayload->value);
+//    cleanupTransferCommandPayload(transferCommandPayload);
+}
+
+void handleFindLessRequest(ClientTransferState *state,PacketPayload *payload){
+    TransferCommandPayload* transferCommandPayload= resolveTransferCommand(state,payload);
+    log_info("IN FUNCTION,ret=%p\n",transferCommandPayload);
+    if(transferCommandPayload==NULL)return;
+    if(!transferCommandPayload->flag){
+        cleanupTransferCommandPayload(transferCommandPayload);
+        log_error("cannot resolve transfer command payload,abort\n");
+        return;
+    }
+
+    log_info("FLAG:TRUE\n");
+
+    if(transferCommandPayload->key==NULL){
+        log_error("In find less request,field key should not be null\n");
+        cleanupTransferCommandPayload(transferCommandPayload);
+        return;
+    }
+
+    if(transferCommandPayload->value!=NULL){
+        log_warning("In find less request,field value should be null\n");
+    }
+
+    log_info("BEGIN_FIND_LESS");
+    void* result=art_find_less(&$__art_tree,transferCommandPayload->key,transferCommandPayload->keyLength);
+
+    log_success("FindLessRequest,key:%s,value:%s\n",transferCommandPayload->key,result,result);
+
+//    cleanupTransferCommandPayload(transferCommandPayload);
+}
+
+
+void handleFindMoreRequest(ClientTransferState *state,PacketPayload *payload){
+
+    TransferCommandPayload *transferCommandPayload= resolveTransferCommand(state,payload);
+    log_info("IN FUNCTION,ret=%p\n",transferCommandPayload);
+    if(transferCommandPayload==NULL)return;
+    if(!transferCommandPayload->flag){
+        cleanupTransferCommandPayload(transferCommandPayload);
+        log_error("cannot resolve transfer command payload,abort\n");
+        return;
+    }
+
+    log_info("FLAG:TRUE\n");
+
+    if(transferCommandPayload->key==NULL){
+        log_error("In find more request,field key should not be null\n");
+        cleanupTransferCommandPayload(transferCommandPayload);
+        return;
+    }
+
+    if(transferCommandPayload->value!=NULL){
+        log_warning("In find more request,field value should be null\n");
+
+    }
+    log_info("BEGIN_FIND_MORE");
+    void* result=art_find_more(&$__art_tree,transferCommandPayload->key,transferCommandPayload->keyLength);
+    log_success("FindMoreRequest,key:%s,value:%s\n",transferCommandPayload->key,result,result);
+
+//    cleanupTransferCommandPayload(transferCommandPayload);
+}
+
+void handleDeleteRequest(ClientTransferState *state,PacketPayload *payload){
+    TransferCommandPayload* transferCommandPayload= resolveTransferCommand(state,payload);
+    log_info("IN FUNCTION,ret=%p\n",transferCommandPayload);
+    if(transferCommandPayload==NULL)return;
+    if(!transferCommandPayload->flag){
+        cleanupTransferCommandPayload(transferCommandPayload);
+        log_error("cannot resolve transfer command payload,abort\n");
+        return;
+    }
+
+    log_info("FLAG:TRUE\n");
+
+    if(transferCommandPayload->key==NULL){
+        log_error("In delete request,field key should not be null\n");
+        cleanupTransferCommandPayload(transferCommandPayload);
+        return;
+    }
+
+    if(transferCommandPayload->value!=NULL){
+        log_warning("In delete request,field value should be null\n");
+    }
+    log_info("BEGIN_DELETE");
+    void* result=art_delete(&$__art_tree,transferCommandPayload->key,transferCommandPayload->keyLength);
+    log_success("DeleteRequest,key:%s,value:%s\n",transferCommandPayload->key,result,result);
+
+//    cleanupTransferCommandPayload(transferCommandPayload);
 }
 
 void initRequestProcessor(void){
+    art_tree_init(&$__art_tree);
     setDataHandler(FIND_REQUEST,handleFindRequest);
-    setDataHandler(SET_REQUEST,handleFindRequest);
-    setDataHandler(UPDATE_REQUEST,handleFindRequest);
-    setDataHandler(FIND_LESS_REQUEST,handleFindRequest);
-    setDataHandler(FIND_MORE_REQUEST,handleFindRequest);
-    setDataHandler(DELETE_REQUEST,handleFindRequest);
+    setDataHandler(SET_REQUEST,handleSetRequest);
+    setDataHandler(UPDATE_REQUEST,handleUpdateRequest);
+    setDataHandler(FIND_LESS_REQUEST,handleFindLessRequest);
+    setDataHandler(FIND_MORE_REQUEST,handleFindMoreRequest);
+    setDataHandler(DELETE_REQUEST,handleDeleteRequest);
 }
