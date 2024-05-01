238bd053390293f67b968a3311350704ddb484dd
Zheng <79322267+Zheng-Niannian@users.noreply.github.com>
2024-05-01 17:55:30 +0800

    Update request_processor.c

--- client/request_processor.c
+++ client/request_processor.c
@@ -1,7 +1,5 @@
 #include"request_processor.h"
 
-art_tree $__art_tree;
-
 void cleanupTransferCommandPayload(TransferCommandPayload *payload) {
     if (payload->key != NULL) {
         free(payload->key);
@@ -54,7 +52,6 @@ TransferCommandPayload *resolveTransferCommand(ClientTransferState *state, Packe
             transferCommandPayload->key = key_buffer;
             fread(key_buffer, 1, transferCommand.keyLength, fp);
             log_info("key:%s\n", key_buffer);
-//            free(key_buffer);
         }
         if (transferCommand.valueLength > 0) {
             unsigned char *value_buffer = malloc(transferCommand.valueLength + 1);
@@ -67,8 +64,6 @@ TransferCommandPayload *resolveTransferCommand(ClientTransferState *state, Packe
             transferCommandPayload->value = value_buffer;
             fread(value_buffer, 1, transferCommand.valueLength, fp);
             log_info("value:%s\n", value_buffer);
-
-//            free(value_buffer);
         }
         fclose(fp);
         fprintf(stdout, "finish process find_request from file\n");
@@ -89,7 +84,6 @@ TransferCommandPayload *resolveTransferCommand(ClientTransferState *state, Packe
             log_info("key:%s\n", key_buffer);
             transferCommandPayload->keyLength = transferCommand.keyLength;
             transferCommandPayload->key = key_buffer;
-//            free(key_buffer);
         }
         char *value = ((char *) payload->src) + sizeof(transferCommand.totalLength) * 3 + transferCommand.keyLength;
         if (transferCommand.valueLength > 0) {
@@ -103,10 +97,7 @@ TransferCommandPayload *resolveTransferCommand(ClientTransferState *state, Packe
             memset(value_buffer, 0, transferCommand.valueLength + 1);
             memcpy(value_buffer, value, transferCommand.valueLength);
             log_info("value:%s\n", value_buffer);
-//            free(value_buffer);
         }
-
-//        fprintf(stdout,"finish process find_request from memory\n");
     }
     transferCommandPayload->flag = 1;
     log_success("Resolve Transfer Command:key:%s, value:%s\n", transferCommandPayload->key,
@@ -114,33 +105,8 @@ TransferCommandPayload *resolveTransferCommand(ClientTransferState *state, Packe
     return transferCommandPayload;
 }
 
-void returnArtResult(ClientTransferState *state,TransferCommandPayload* memoryPayload,const char* operation_name,enum CommandType type){
 
-    char *retBuffer= serializeTransferData(memoryPayload,0,type);
-    if(!retBuffer){
-        log_error("cannot allocate memory for return %s result\n",operation_name);
-        return;
-    }
-    sendSerializeData(state,retBuffer,3*sizeof(PacketHeader));
-    free(retBuffer);
-    sendRawData(state,memoryPayload->key,memoryPayload->keyLength);
-    sendRawData(state,memoryPayload->value,memoryPayload->valueLength);
-}
-
-void returnEmptyArtResult(ClientTransferState *state,const char* operation_name,enum CommandType type){
-    TransferCommandPayload empty={
-            0,0,0,NULL,NULL
-    };
-    char *retBuffer= serializeTransferData(&empty,0,type);
-    if(!retBuffer){
-        log_error("cannot allocate memory for return %s result\n",operation_name);
-        return;
-    }
-    sendSerializeData(state,retBuffer,3*sizeof(PacketHeader));
-    free(retBuffer);
-}
-
-void handleFindRequest(ClientTransferState *state, PacketPayload *payload) {
+void handleFindResult(ClientTransferState *state, PacketPayload *payload) {
     TransferCommandPayload *transferCommandPayload = resolveTransferCommand(state, payload);
     log_info("IN FUNCTION,ret=%p\n", transferCommandPayload);
     if (transferCommandPayload == NULL)return;
@@ -151,29 +117,16 @@ void handleFindRequest(ClientTransferState *state, PacketPayload *payload) {
         return;
     }
 
-    if (transferCommandPayload->key == NULL) {
-        log_error("In find request,field key should not be null\n");
-        cleanupTransferCommandPayload(transferCommandPayload);
-        return;
-    }
+    log_info("RECEIVE_FIND\n");
 
-    if (transferCommandPayload->value != NULL) {
-        log_warning("In find request,field value should be null\n");
-        //not necessary for free
-    }
-    log_info("BEGIN_FIND\n");
-    TransferCommandPayload *memoryPayload =(TransferCommandPayload*) art_search(&$__art_tree, transferCommandPayload->key, transferCommandPayload->keyLength);
+    log_success("keyLength:%d\nkey:%s\nValueLength:%d\nvalue:%s\n", transferCommandPayload->keyLength,
+                transferCommandPayload->key,
+                transferCommandPayload->valueLength, transferCommandPayload->value);
 
-    log_success("FindRequest,key:%s, find:%d\n", transferCommandPayload->key, memoryPayload!=NULL);
-    if(memoryPayload!=NULL){
-        log_success("In memory,key:%s, value:%s\n",memoryPayload->key,memoryPayload->value);
-        returnArtResult(state,memoryPayload,"find",FIND_RESULT);
-    }else{
-        returnEmptyArtResult(state,"find",FIND_RESULT);
-    }
+    cleanupTransferCommandPayload(transferCommandPayload);
 }
 
-void handleSetRequest(ClientTransferState *state, PacketPayload *payload) {
+void handleSetResult(ClientTransferState *state, PacketPayload *payload) {
     TransferCommandPayload *transferCommandPayload = resolveTransferCommand(state, payload);
     log_info("IN FUNCTION,ret=%p\n", transferCommandPayload);
     if (transferCommandPayload == NULL)return;
@@ -183,35 +136,16 @@ void handleSetRequest(ClientTransferState *state, PacketPayload *payload) {
         return;
     }
 
-    if (transferCommandPayload->key == NULL) {
-        log_error("In set request,field key should not be null\n");
-        cleanupTransferCommandPayload(transferCommandPayload);
-        return;
-    }
+    log_info("RECEIVE_SET\n");
 
-    if (transferCommandPayload->value == NULL) {
-        log_error("In set request,field value should not be null\n");
-        cleanupTransferCommandPayload(transferCommandPayload);
-        return;
-    }
-    log_info("BEGIN_SET\n");
-    TransferCommandPayload *memoryPayload =(TransferCommandPayload*) art_insert_no_replace(&$__art_tree, transferCommandPayload->key, transferCommandPayload->keyLength,
-                                         (void *) transferCommandPayload);
+    log_success("keyLength:%d\nkey:%s\nValueLength:%d\nvalue:%s\n", transferCommandPayload->keyLength,
+                transferCommandPayload->key,
+                transferCommandPayload->valueLength, transferCommandPayload->value);
 
-    log_success("SetRequest,key:%s, exist previous data:%d\n", transferCommandPayload->key, memoryPayload!=NULL);
-    if(memoryPayload!=NULL){
-        log_success("In memory, previous key:%s, previous value:%s\n",memoryPayload->key,memoryPayload->value);
-        returnArtResult(state,memoryPayload,"set",SET_RESULT);
-        cleanupTransferCommandPayload(transferCommandPayload);
-    }else{
-        returnEmptyArtResult(state,"set",SET_RESULT);
-//
-    }
-
-
+    cleanupTransferCommandPayload(transferCommandPayload);
 }
 
-void handleUpdateRequest(ClientTransferState *state, PacketPayload *payload) {
+void handleUpdateResult(ClientTransferState *state, PacketPayload *payload) {
     TransferCommandPayload *transferCommandPayload = resolveTransferCommand(state, payload);
     log_info("IN FUNCTION,ret=%p\n", transferCommandPayload);
     if (transferCommandPayload == NULL)return;
@@ -221,31 +155,15 @@ void handleUpdateRequest(ClientTransferState *state, PacketPayload *payload) {
         return;
     }
 
-    if (transferCommandPayload->key == NULL) {
-        log_error("In update request,field key should not be null\n");
-        cleanupTransferCommandPayload(transferCommandPayload);
-        return;
-    }
 
-    if (transferCommandPayload->value == NULL) {
-        log_error("In update request,field value should not be null\n");
-        cleanupTransferCommandPayload(transferCommandPayload);
-        return;
-    }
-    log_info("BEGIN_UPDATE\n");
-    TransferCommandPayload *memoryPayload =(TransferCommandPayload*) art_insert(&$__art_tree, transferCommandPayload->key, transferCommandPayload->keyLength,
-                              transferCommandPayload);
-    log_success("UpdateRequest,key:%s, exist previous data:%d\n", transferCommandPayload->key, memoryPayload!=NULL);
-    if(memoryPayload!=NULL){
-        log_success("In memory, previous key:%s, previous value:%s\n",memoryPayload->key,memoryPayload->value);
-        returnArtResult(state,memoryPayload,"update",UPDATE_RESULT);
-        cleanupTransferCommandPayload(memoryPayload);
-    }else{
-        returnEmptyArtResult(state,"update",UPDATE_RESULT);
-    }
+    log_info("RECEIVE_UPDATE\n");
+    log_success("keyLength:%d\nkey:%s\nValueLength:%d\nvalue:%s\n", transferCommandPayload->keyLength,
+                transferCommandPayload->key,
+                transferCommandPayload->valueLength, transferCommandPayload->value);
+    cleanupTransferCommandPayload(transferCommandPayload);
 }
 
-void handleFindLessRequest(ClientTransferState *state, PacketPayload *payload) {
+void handleFindLessResult(ClientTransferState *state, PacketPayload *payload) {
     TransferCommandPayload *transferCommandPayload = resolveTransferCommand(state, payload);
     log_info("IN FUNCTION,ret=%p\n", transferCommandPayload);
     if (transferCommandPayload == NULL)return;
@@ -255,30 +173,16 @@ void handleFindLessRequest(ClientTransferState *state, PacketPayload *payload) {
         return;
     }
 
-    if (transferCommandPayload->key == NULL) {
-        log_error("In find less request,field key should not be null\n");
-        cleanupTransferCommandPayload(transferCommandPayload);
-        return;
-    }
 
-    if (transferCommandPayload->value != NULL) {
-        log_warning("In find less request,field value should be null\n");
-    }
-
-    log_info("BEGIN_FIND_LESS\n");
-    TransferCommandPayload *memoryPayload =(TransferCommandPayload*) art_find_less(&$__art_tree, transferCommandPayload->key, transferCommandPayload->keyLength);
-
-    log_success("FindLessRequest,key:%s, exist data:%d\n", transferCommandPayload->key, memoryPayload!=NULL);
-    if(memoryPayload!=NULL){
-        log_success("In memory, key:%s, value:%s\n",memoryPayload->key,memoryPayload->value);
-        returnArtResult(state,memoryPayload,"find_less",FIND_LESS_RESULT);
-    }else{
-        returnEmptyArtResult(state,"find_less",FIND_LESS_RESULT);
-    }
+    log_info("RECEIVE_FIND_LESS\n");
+    log_success("keyLength:%d\nkey:%s\nValueLength:%d\nvalue:%s\n", transferCommandPayload->keyLength,
+                transferCommandPayload->key,
+                transferCommandPayload->valueLength, transferCommandPayload->value);
+    cleanupTransferCommandPayload(transferCommandPayload);
 }
 
 
-void handleFindMoreRequest(ClientTransferState *state, PacketPayload *payload) {
+void handleFindMoreResult(ClientTransferState *state, PacketPayload *payload) {
     TransferCommandPayload *transferCommandPayload = resolveTransferCommand(state, payload);
     log_info("IN FUNCTION,ret=%p\n", transferCommandPayload);
     if (transferCommandPayload == NULL)return;
@@ -288,28 +192,17 @@ void handleFindMoreRequest(ClientTransferState *state, PacketPayload *payload) {
         return;
     }
 
-    if (transferCommandPayload->key == NULL) {
-        log_error("In find more request,field key should not be null\n");
-        cleanupTransferCommandPayload(transferCommandPayload);
-        return;
-    }
 
-    if (transferCommandPayload->value != NULL) {
-        log_warning("In find more request,field value should be null\n");
+    log_info("RECEIVE_FIND_MORE\n");
 
-    }
-    log_info("BEGIN_FIND_MORE\n");
-    TransferCommandPayload *memoryPayload =(TransferCommandPayload*) art_find_more(&$__art_tree, transferCommandPayload->key, transferCommandPayload->keyLength);
-    log_success("FindMoreRequest,key:%s, exist data:%d\n", transferCommandPayload->key, memoryPayload!=NULL);
-    if(memoryPayload!=NULL){
-        log_success("In memory, key:%s, value:%s\n",memoryPayload->key,memoryPayload->value);
-        returnArtResult(state,memoryPayload,"find_more",FIND_MORE_RESULT);
-    }else{
-        returnEmptyArtResult(state,"find_more",FIND_MORE_RESULT);
-    }
+    log_success("keyLength:%d\nkey:%s\nValueLength:%d\nvalue:%s\n", transferCommandPayload->keyLength,
+                transferCommandPayload->key,
+                transferCommandPayload->valueLength, transferCommandPayload->value);
+
+    cleanupTransferCommandPayload(transferCommandPayload);
 }
 
-void handleDeleteRequest(ClientTransferState *state, PacketPayload *payload) {
+void handleDeleteResult(ClientTransferState *state, PacketPayload *payload) {
     TransferCommandPayload *transferCommandPayload = resolveTransferCommand(state, payload);
     log_info("IN FUNCTION,ret=%p\n", transferCommandPayload);
     if (transferCommandPayload == NULL)return;
@@ -319,53 +212,30 @@ void handleDeleteRequest(ClientTransferState *state, PacketPayload *payload) {
         return;
     }
 
-    if (transferCommandPayload->key == NULL) {
-        log_error("In delete request,field key should not be null\n");
-        cleanupTransferCommandPayload(transferCommandPayload);
-        return;
-    }
 
-    if (transferCommandPayload->value != NULL) {
-        log_warning("In delete request,field value should be null\n");
-    }
-    log_info("BEGIN_DELETE\n");
-    TransferCommandPayload *memoryPayload =(TransferCommandPayload*) art_delete(&$__art_tree, transferCommandPayload->key, transferCommandPayload->keyLength);
-    log_success("DeleteRequest,key:%s, exist data:%d\n", transferCommandPayload->key, memoryPayload!=NULL);
-    if(memoryPayload!=NULL){
-        log_success("In memory, key:%s, value:%s\n",memoryPayload->key,memoryPayload->value);
-        returnArtResult(state,memoryPayload,"delete",DELETE_RESULT);
-        cleanupTransferCommandPayload(memoryPayload);
-    }else{
-        returnEmptyArtResult(state,"delete",DELETE_RESULT);
-    }
+    log_info("RECEIVE_DELETE\n");
+    log_success("keyLength:%d\nkey:%s\nValueLength:%d\nvalue:%s\n", transferCommandPayload->keyLength,
+                transferCommandPayload->key,
+                transferCommandPayload->valueLength, transferCommandPayload->value);
+
+    cleanupTransferCommandPayload(transferCommandPayload);
 }
 
-void handleSaveRequest(ClientTransferState *state,PacketPayload *payload){
-    SaveRequest *request=(SaveRequest*)payload->src;
-    SaveResult result={
-            0
-    };
-    if(request->flag){
-        if(save_art_data(&$__art_tree)){
-            log_success("Save Data Success\n");
-            result.flag=1;
-        }else{
-            log_error("Save Data Failed\n");
-        }
+void handleSaveResult(ClientTransferState *state,PacketPayload *payload){
+    SaveResult *result=(SaveResult*)payload->src;
+    if(result->flag){
+        log_success("Server save data success\n");
+    }else{
+        log_error("Server save data failed\n");
     }
-    char* buffer=serializeTransferData(&result,sizeof(SaveResult),SAVE_RESULT);
-    sendSerializeData(state,buffer,sizeof(SaveResult));
-    free(buffer);
 }
 
 void initRequestProcessor(void) {
-    art_tree_init(&$__art_tree);
-    load_art_data(&$__art_tree);
-    setDataHandler(FIND_REQUEST, handleFindRequest);
-    setDataHandler(SET_REQUEST, handleSetRequest);
-    setDataHandler(UPDATE_REQUEST, handleUpdateRequest);
-    setDataHandler(FIND_LESS_REQUEST, handleFindLessRequest);
-    setDataHandler(FIND_MORE_REQUEST, handleFindMoreRequest);
-    setDataHandler(DELETE_REQUEST, handleDeleteRequest);
-    setDataHandler(SAVE_REQUEST,handleSaveRequest);
+    setDataHandler(FIND_RESULT, handleFindResult);
+    setDataHandler(SET_RESULT, handleSetResult);
+    setDataHandler(UPDATE_RESULT, handleUpdateResult);
+    setDataHandler(FIND_LESS_RESULT, handleFindLessResult);
+    setDataHandler(FIND_MORE_RESULT, handleFindMoreResult);
+    setDataHandler(DELETE_RESULT, handleDeleteResult);
+    setDataHandler(SAVE_RESULT,handleSaveResult);
 }
